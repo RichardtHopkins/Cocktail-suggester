35081a5bb099f984c24d23f58d8b29c0279b3dc5
Richard Hopkins <richard.t.hopkins@outlook.com>
2021-04-08 19:38:51 +1000

    Pseudo code

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+//fetch the list of drinks with ingredient that is searched.
+    //add drinks to the array using .push();
+    //for fetch json array.
+        //if(ingreent is included)
+            //add to page
+        //else forget about it
+    
+//fetch places with a range.
+    //return a list of stores
+    //same as above
+
+//function to display list of drinks with ingredient in them
+    //fetch for image
+    //get the details from the object array that we create
+    //display name, image and ingredients as a card
+
+//click on a card
+    //display modal with cocktail name, instruction, ingredients , image, stores near by
+
+// make search bar fixed to bottom after search. 
