00a21568f340690b52608da82c88d65595c2535a
Tom Chappell <thomas.chappell@outlook.com>
2021-04-18 23:10:24 +1000

    made changes to make merging easier

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,17 @@ var searchButton = $("#searchBtn");
 var apiKey = "1";
 var userIngredient;
 var drinkCardList = [];
-var drinkCardIngredients = []
 var Cocktailingredients = []
+var logo = $("#logo")
+var input=$("#searchBar")
+var formEl = $("#ingredient-form")
+
+//declares the array and sets it in local storage.
+var recentSearches = [];
+if(!recentSearches){
+    recentSearches.push(JSON.parse(localStorage.getItem('history')));
+    localStorage.setItem('history', JSON.stringify(recentSearches));
+}
 
 //loop for persisting the data onto HMTL page
 for (var i = 0; i < localStorage.length; i++) {
@@ -19,6 +28,12 @@ var keyCount = 0;
 
 //search function on button click
 searchButton.click(function () {
+    //clears current card list
+    $("#cocktail-card-element").html('')
+    //hiding logo and moving search section
+    logo.addClass("hide")
+    formEl.attr("class","ingredient-form-results")
+    formEl.children().css("margin","0.5rem")
     //initialising ingredient search data
     userIngredient = $("#searchBar").val();
     var ingredientNameUrl = "https://www.thecocktaildb.com/api/json/v1/" + apiKey + "/filter.php?i=" + userIngredient;
@@ -116,7 +131,46 @@ searchButton.click(function () {
         }(i)
         }
     });
+    //saves search to local storage.
+    saveSearch();
+    createBtns();
+    console.log(drinkCardList)
 });
+
+function saveSearch(){
+   
+    //gets the value of the search bar and assigns it to latest search
+    var latestSearch = $('#searchBar').val().trim();
+
+    //check to see if the search is null. if it is console log that there was no search.
+    if(latestSearch && !recentSearches.includes(latestSearch)){
+       recentSearches = JSON.parse(localStorage.getItem('history')) || [];                     //get the previouse searches object and store it in history
+       recentSearches.push(latestSearch);                                                      // push the value from the search bar to the recent searches array.
+       localStorage.setItem('history', JSON.stringify(recentSearches));                        //set the local storage to be the new array.
+
+       //creates a new button and adds it to the top of the recent search list.
+       $('#searchHistory').prepend('<div class="p-2"><button type="button" class="btn-primary histBtn">' + latestSearch +'</button></div>');
+       
+    }else{
+        console.log('nothing entered.')
+    }
+
+    
+ 
+};
+
+//creates recent search buttons on page load.
+function createBtns(){
+   recentSearches = JSON.parse(localStorage.getItem('history')) || [];
+   for(var i = 0; i <= recentSearches.length -1; i++){
+       if(recentSearches){
+           $('#searchHistory').prepend('<div class="p-2 form-group"><button type="button" class="btn-primary histBtn">' + recentSearches[i] +'</button></div>');  
+       }
+   }
+}
+
+
+createBtns();
 
 // // this.setState({ cocktail })
 // // console.log(cocktail);
